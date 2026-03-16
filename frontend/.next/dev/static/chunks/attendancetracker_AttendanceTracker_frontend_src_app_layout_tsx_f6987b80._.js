ed7663929d3b597462c16fd6d488fc27157251ef
vivabaranwal <vivacodes03@gmail.com>
2026-03-17 01:51:53 +0530

    dark mode and responsiveness

--- frontend/.next/dev/static/chunks/attendancetracker_AttendanceTracker_frontend_src_app_layout_tsx_f6987b80._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__5de076da._.css",
-  "static/chunks/7cca3_sonner_dist_index_mjs_1b3d96fc._.js"
-],
-    source: "dynamic"
-});
